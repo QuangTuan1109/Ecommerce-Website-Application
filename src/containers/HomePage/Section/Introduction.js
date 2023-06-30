4c14bb038c40d45b828796d956e5e3af412488b4
QuangTuan1109 <74809953+QuangTuan1109@users.noreply.github.com>
2023-06-30 14:42:59 +0700

    update slider

--- src/containers/HomePage/Section/Introduction.js
+++ src/containers/HomePage/Section/Introduction.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Introduction.scss'
-import img2 from '../../../assets/introduction/promotions 2.gif'
 
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css"
