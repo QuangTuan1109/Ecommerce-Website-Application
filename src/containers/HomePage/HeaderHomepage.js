c0e2a765e3b069a1edb9a66a9841a766482f37dd
QuangTuan1109 <74809953+QuangTuan1109@users.noreply.github.com>
2023-07-30 15:46:33 +0700

    update homepage

--- src/containers/HomePage/HeaderHomepage.js
+++ src/containers/HomePage/HeaderHomepage.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Link  } from "react-router-dom";
 import { connect } from 'react-redux';
 import './HeaderHomepage.scss'
 import avt from '../../assets/images/avatar.png'
 import logo from '../../assets/images/logo-website.png'
+import user from '../../assets/icon/Sample_User_Icon.png'
+import settings from '../../assets/icon/Sample_Settings_Icon.png'
+import Theme from '../../assets/icon/Sample_Theme_Icon.png'
+import Helps from '../../assets/icon/Sample_Help_Icon.png'
+import Feedback from '../../assets/icon/Sample_Feedback_Icon.png'
+import Logout from '../../assets/icon/Sample_logout_Icon.png'
 
 class HeaderHomepage extends Component {
     constructor(props) {
@@ -17,7 +23,7 @@ class HeaderHomepage extends Component {
 
     handleChange(event) {
         this.setState({value: event.target.value});
-      }
+    }
 
     render() {
         return (
@@ -62,11 +68,59 @@ class HeaderHomepage extends Component {
                                 <div className='cart'>
                                     <Link to='/' className='component-link'><i class="fas fa-heart"></i></Link>
                                     <Link to='/' className='component-link'><i class="fas fa-shopping-cart"></i></Link>
-                                    <Link to='/' className='component-link'><i class="fas fa-bell"></i></Link>
+                                    <div className='notification-section'>
+                                        <div className='notification-icon'>
+                                            <i class="fas fa-bell"></i>
+                                        </div>
+                                        <div className='notification'>
+                                            <h3>Notification</h3>
+                                            <div className='notification-element'>
+                                                <img src={avt} alt={'avt'}/>
+                                                <div className='content-notification'>
+                                                    <Link to='/' className='content-noti'>Lê Quang Tuấn posted a new post: New shirt in 2023</Link>
+                                                    <small>2h ago</small>
+                                                </div>
+                                            </div>
+                                            <div className='notification-element'>
+                                                <img src={avt} alt={'avt'}/>
+                                                <div className='content-notification'>
+                                                    <Link to='/' className='content-noti'>Lê Quang Tuấn posted a new post: New shirt in 2023</Link>
+                                                    <small>2h ago</small>
+                                                </div>
+                                            </div>
+                                            <div className='notification-element'>
+                                                <img src={avt} alt={'avt'}/>
+                                                <div className='content-notification'>
+                                                    <Link to='/' className='content-noti'>Lê Quang Tuấn posted a new post: New shirt in 2023</Link>
+                                                    <small>2h ago</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div className='vertical-line'></div>
                                 <div className='avatar-image'>
-                                    <img src={avt} alt={'avt'}/>
+                                    <div className='avt'>
+                                        <img src={avt} alt={'avt'}/>
+                                    </div>
+                                    <div className='menu'>
+                                        <h3>Lê Quang Tuấn</h3>
+                                        <p>Customer</p>
+                                        <ul>
+                                            <li><img src={user} alt={'user-icon'} /><Link to='/' className='menu-element'>My Profile</Link></li>
+                                            <li><img src={settings} alt={'settings-icon'} /><Link to='/' className='menu-element'>Settings</Link></li>
+                                            <li>
+                                                <img src={Theme} alt={'Theme-icon'} /><Link to='/' className='menu-element'>Theme</Link>
+                                                <label class="switch">
+                                                    <input type="checkbox" />
+                                                    <span class="slider round"></span>
+                                                </label>
+                                            </li>
+                                            <li><img src={Helps} alt={'Helps-icon'} /><Link to='/' className='menu-element'>Helps</Link></li>
+                                            <li><img src={Feedback} alt={'user-icon'} /><Link to='/' className='menu-element'>Send Feedback</Link></li>
+                                            <li><img src={Logout} alt={'Logout-icon'} /><Link to='/' className='menu-element'>Log out</Link></li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
