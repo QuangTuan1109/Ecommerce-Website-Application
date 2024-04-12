0fe0964bb86c185e8e3d78534587cbfa168cfff1
QuangTuan1109 <74809953+QuangTuan1109@users.noreply.github.com>
2024-04-12 16:35:05 +0700

    update

--- src/containers/Seller/ProductPageSeller/CreateProduct/CreateProduct.js
+++ src/containers/Seller/ProductPageSeller/CreateProduct/CreateProduct.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './CreateProduct.scss';
 
-import Header from '../../Header'
-import Navbar from './Section/navBar'
-import BasicInformation from './Section/BasicInformation'
-import DetailInformation from './Section/DetailInformation'
-import SellInformation from './Section/SellInformation'
-import Delivery from './Section/Delivery'
-import OrtherInformation from './Section/OrtherInformation'
+import Header from '../../Header';
+import Navbar from './Section/navBar';
+import BasicInformation from './Section/BasicInformation';
+import DetailInformation from './Section/DetailInformation';
+import SellInformation from './Section/SellInformation';
+import Delivery from './Section/Delivery';
+import OrtherInformation from './Section/OrtherInformation';
 
+/**
+ * Component for creating a new product.
+ * Displays forms for basic, detail, sell, delivery, and other information of the product.
+ */
 class CreateProduct extends Component {
     constructor(props) {
         super(props);
+        // Initialize component state
         this.state = {
-            // các trạng thái khác của component
-            selectedCategories: null, // lưu trữ ngành hàng đã chọn
-            isCategorySelected: false, // xác định xem người dùng đã chọn ngành hàng hay chưa
+            selectedCategories: null, // Stores the selected categories
+            isCategorySelected: false, // Determines if the user has selected a category or not
         };
     }
 
+    /**
+     * Handles the selection of categories.
+     * @param {Array} selectedCategories - Array of selected categories.
+     */
     handleCategorySelect = (selectedCategories) => {
         this.setState({ selectedCategories, isCategorySelected: true });
     }
@@ -36,22 +44,29 @@ class CreateProduct extends Component {
                     </div>
                     <div className="right-content">
                         <div className="section">
+                            {/* Render BasicInformation component and pass handleCategorySelect function as prop */}
                             <BasicInformation onCategorySelect={this.handleCategorySelect} />
                         </div>
+                        {/* Render other sections if a category is selected */}
                         {isCategorySelected ? (
                             <>
                                 <div className="section">
+                                    {/* Render DetailInformation component and pass selectedCategories as prop */}
                                     <DetailInformation selectedCategories={selectedCategories} />
                                 </div>
                                 <div className="section">
+                                    {/* Render SellInformation component and pass selectedCategories as prop */}
                                     <SellInformation selectedCategories={selectedCategories} />
                                 </div>
                                 <div className="section">
+                                    {/* Render Delivery component and pass selectedCategories as prop */}
                                     <Delivery selectedCategories={selectedCategories} />
                                 </div>
                                 <div className="section">
+                                    {/* Render OrtherInformation component and pass selectedCategories as prop */}
                                     <OrtherInformation selectedCategories={selectedCategories} />
                                 </div>
+                                {/* Render buttons for saving and canceling */}
                                 <div className="buttons-container">
                                     <button className="cancel-button">Hủy</button>
                                     <button className="save-hide-button">Lưu & Ẩn</button>
@@ -59,29 +74,32 @@ class CreateProduct extends Component {
                                 </div>
                             </>
                         ) : 
+                        // Render placeholder sections if no category is selected
                         <>
-                                <div className="section">
-                                    <h2>Thông tin chi tiết</h2>
-                                    <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
-                                </div>
-                                <div className="section">
+                            <div className="section">
+                                <h2>Thông tin chi tiết</h2>
+                                <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
+                            </div>
+                            <div className="section">
                                 <h2>Thông tin bán hàng</h2>
-                                    <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
-                                </div>
-                                <div className="section">
+                                <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
+                            </div>
+                            <div className="section">
                                 <h2>Vận chuyển</h2>
-                                    <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
-                                </div>
-                                <div className="section">
+                                <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
+                            </div>
+                            <div className="section">
                                 <h2>Thông tin khác</h2>
-                                    <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
-                                </div>
-                                <div className="buttons-container">
-                                    <button className="cancel-button" disabled={true}>Hủy</button>
-                                    <button className="save-hide-button" disabled={true}>Lưu & Ẩn</button>
-                                    <button className="save-show-button" disabled={true}>Lưu & Hiển thị</button>
-                                </div>
-                            </>}
+                                <p>Có thể điều chỉnh sau khi chọn ngành hàng.</p>
+                            </div>
+                            {/* Render disabled buttons for saving and canceling */}
+                            <div className="buttons-container">
+                                <button className="cancel-button" disabled={true}>Hủy</button>
+                                <button className="save-hide-button" disabled={true}>Lưu & Ẩn</button>
+                                <button className="save-show-button" disabled={true}>Lưu & Hiển thị</button>
+                            </div>
+                        </>
+                        }
                     </div>
                 </div>
             </div>
